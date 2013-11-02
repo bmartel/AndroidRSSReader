71043a4edc21dae0cefc1dbbbc9b246beda8bc63
bmartel <brandonmartel@gmail.com>
2013-11-02 03:54:14 -0500

    Added Settings Activity

--- gen/com/kryonation/androidrssreader/R.java
+++ gen/com/kryonation/androidrssreader/R.java
@@ -9,7 +9,11 @@ package com.kryonation.androidrssreader;
 
 public final class R {
     public static final class array {
-        public static final int nhl_feeds=0x7f060000;
+        public static final int nhl_feeds=0x7f070000;
+        public static final int pref_example_list_titles=0x7f070001;
+        public static final int pref_example_list_values=0x7f070002;
+        public static final int pref_sync_frequency_titles=0x7f070003;
+        public static final int pref_sync_frequency_values=0x7f070004;
     }
     public static final class attr {
     }
@@ -20,8 +24,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -33,19 +37,19 @@ public final class R {
         public static final int unstarred=0x7f020006;
     }
     public static final class id {
-        public static final int ScrollView=0x7f090002;
-        public static final int actionbar_markunread=0x7f09000a;
-        public static final int actionbar_refresh=0x7f09000c;
-        public static final int actionbar_saveoffline=0x7f09000b;
-        public static final int article_author=0x7f090005;
-        public static final int article_detail=0x7f090006;
-        public static final int article_detail_container=0x7f090000;
-        public static final int article_detail_layout=0x7f090003;
-        public static final int article_list=0x7f090001;
-        public static final int article_listing_smallprint=0x7f090009;
-        public static final int article_row_layout=0x7f090007;
-        public static final int article_title=0x7f090004;
-        public static final int article_title_text=0x7f090008;
+        public static final int ScrollView=0x7f0a0002;
+        public static final int actionbar_markunread=0x7f0a000a;
+        public static final int actionbar_refresh=0x7f0a000c;
+        public static final int actionbar_saveoffline=0x7f0a000b;
+        public static final int article_author=0x7f0a0005;
+        public static final int article_detail=0x7f0a0006;
+        public static final int article_detail_container=0x7f0a0000;
+        public static final int article_detail_layout=0x7f0a0003;
+        public static final int article_list=0x7f0a0001;
+        public static final int article_listing_smallprint=0x7f0a0009;
+        public static final int article_row_layout=0x7f0a0007;
+        public static final int article_title=0x7f0a0004;
+        public static final int article_title_text=0x7f0a0008;
     }
     public static final class layout {
         public static final int activity_article_detail=0x7f030000;
@@ -55,19 +59,47 @@ public final class R {
         public static final int fragment_article_list=0x7f030004;
     }
     public static final class menu {
-        public static final int detailmenu=0x7f080000;
-        public static final int refreshmenu=0x7f080001;
+        public static final int detailmenu=0x7f090000;
+        public static final int refreshmenu=0x7f090001;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int btn_mark_unread=0x7f050004;
-        public static final int btn_offline_reading=0x7f050003;
-        public static final int btn_refresh=0x7f050005;
-        public static final int title_article_detail=0x7f050001;
-        public static final int title_article_list=0x7f050002;
+        public static final int app_name=0x7f060000;
+        public static final int btn_mark_unread=0x7f060004;
+        public static final int btn_offline_reading=0x7f060003;
+        public static final int btn_refresh=0x7f060005;
+        public static final int pref_default_display_name=0x7f06000b;
+        public static final int pref_description_social_recommendations=0x7f060009;
+        /**  Example settings for Data & Sync 
+         */
+        public static final int pref_header_data_sync=0x7f06000d;
+        /**  Strings related to Settings 
+ Example General settings 
+         */
+        public static final int pref_header_general=0x7f060007;
+        /**  Example settings for Notifications 
+         */
+        public static final int pref_header_notifications=0x7f060010;
+        public static final int pref_ringtone_silent=0x7f060013;
+        public static final int pref_title_add_friends_to_messages=0x7f06000c;
+        public static final int pref_title_display_name=0x7f06000a;
+        public static final int pref_title_new_message_notifications=0x7f060011;
+        public static final int pref_title_ringtone=0x7f060012;
+        public static final int pref_title_social_recommendations=0x7f060008;
+        public static final int pref_title_sync_frequency=0x7f06000e;
+        public static final int pref_title_system_sync_settings=0x7f06000f;
+        public static final int pref_title_vibrate=0x7f060014;
+        public static final int title_activity_settings=0x7f060006;
+        public static final int title_article_detail=0x7f060001;
+        public static final int title_article_list=0x7f060002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
-        public static final int MyActionBar=0x7f070001;
+        public static final int AppTheme=0x7f080000;
+        public static final int MyActionBar=0x7f080001;
+    }
+    public static final class xml {
+        public static final int pref_data_sync=0x7f040000;
+        public static final int pref_general=0x7f040001;
+        public static final int pref_headers=0x7f040002;
+        public static final int pref_notification=0x7f040003;
     }
 }
